63a656efeb13d947d7e9e35cddd3010ca6f8b01a
Anne Bouccin <anne.bouccin@gmail.com>
2020-04-29 12:12:48 +0200

    B-feature[back]-Initialization BDD

---
+++ backend/src/Entity/Theme.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ThemeRepository")
+ */
+class Theme
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $identifier;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getIdentifier(): ?int
+    {
+        return $this->identifier;
+    }
+
+    public function setIdentifier(?int $identifier): self
+    {
+        $this->identifier = $identifier;
+
+        return $this;
+    }
+}
