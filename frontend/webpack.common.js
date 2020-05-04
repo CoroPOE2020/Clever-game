4511b56410ca78f41353eb4688435895de2b2d54
vreymond <valentinreymond@gmail.com>
2020-05-04 10:28:50 +0200

    F - webpack/feature[webpack] Creating webpack structure

---
+++ frontend/webpack.common.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        main: './src/app.js'
+    },
+    target: 'node',
+    externals: [nodeExternals()],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+};
