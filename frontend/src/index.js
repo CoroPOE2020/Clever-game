68ae7fd9be5cd69042e906575bbc5d59362d99a5
Clement Franch <glukor@hotmail.fr>
2020-05-12 11:51:19 +0200

    F - redux store[react-structure] - Creating Redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider, connect } from "react-redux";
 
-// import store from "./store/store";
+import store from "./store/store";
 
 import App from "./components/App";
 
 // import styles
 import './scss/main.scss';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
