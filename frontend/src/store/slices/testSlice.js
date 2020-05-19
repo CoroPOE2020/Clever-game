33ca91bb9ac26f21dc457202ab8cf94abb366e2d
Clement Franch <glukor@hotmail.fr>
2020-05-19 15:08:54 +0200

    F - fixTestSlice[redux] - Fixing testSlice for building

--- frontend/src/store/slices/testSlice.js
+++ frontend/src/store/slices/testSlice.js
@@ -33,17 +33,16 @@ export const fetchPokemonData = (pokemonName) => {
     console.log(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     // TODO dispatch(fetchPokemonDataRequest); // => { loading: true } pour le spinner
     return function (dispatch) {
-        return
-            fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-                .then(response => response.json()) // data
-                .then(data => {
-                    console.log(data);
-                    dispatch(fetchPokemonDataSuccess(data));
-                })
-                .catch(err => {
-                    console.log(err);
-                    dispatch(fetchPokemonDataFailed({ err: err, message: 'Api calls failed' }));
-                });
+        return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+            .then(response => response.json()) // data
+            .then(data => {
+                console.log(data);
+                dispatch(fetchPokemonDataSuccess(data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(fetchPokemonDataFailed({ err: err, message: 'Api calls failed' }));
+            });
     };
 }
 
