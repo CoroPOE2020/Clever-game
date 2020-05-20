3ac31dd22cb0bddb587eb5258be49c1d383a2be7
Fatoorehchi <tfatoo@gmail.com>
2020-05-20 14:27:21 +0200

    F - InstallPackage[redux] - Package Redux Logger explicite Redux interaction

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,17 +3,21 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import testSlice from './slices/testSlice';
 import { checkAgePositive } from '../middleware/redux/checkAgePositive';
 
+import { createLogger } from 'redux-logger';
 
-// const middleware = [
-//     ...getDefaultMiddleware(),
-//     checkAgePositive
-// ];
+const logger = createLogger({});
+
+const middleware = [
+    ...getDefaultMiddleware(),
+    logger
+    //checkAgePositive
+];
 
 const store = configureStore({
     reducer: {
         test: testSlice
     },
-    //middleware
+    middleware
 });
 
 export default store;
