060b2bf688b76947395d3040b554e1f36e151f9d
vreymond <valentinreymond@gmail.com>
2020-05-25 10:23:36 +0200

    F - content-init[content-component] - Content initialization

---
+++ frontend/src/components/Content/Content.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+
+const Content = props => {
+    return (
+        <Fragment>
+            <p>Header</p>
+            {/* Header / Main pages / Footer*/}
+        </Fragment>
+    );
+};
+
+export default Content;
