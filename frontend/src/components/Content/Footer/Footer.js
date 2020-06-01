6a3c27c325e7bba1b58123b2e7b9dae03c82e3f4
vreymond <valentinreymond@gmail.com>
2020-06-01 11:44:04 +0200

    F - footer[header-component] - Footer component + MenuFooter component

---
+++ frontend/src/components/Content/Footer/Footer.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from "react";
+import  { isMobileOnly, isMobile } from 'react-device-detect';
+
+import Menu from '../../UI/Menu/Menu';
+
+// let displayMenu = isMobileOnly ? <Menu /> : null;
+
+
+let displayMenu = null;
+let displayPlusOption = null;
+
+if (isMobileOnly) {
+    displayMenu = <Menu/>;
+    displayPlusOption = <Plus />
+}
+else {
+    displayMenu = <MenuFooter/>
+}
+ 
+
+const Footer = props => {
+    return (
+        <Fragment>
+            <footer className="footer">
+                { displayMenu }
+                { displayPlusOption }
+            </footer>
+        </Fragment>
+    );
+};
+
+export default Footer;
