22357519bfa8313c74e15cd0de9f8d0080d4138a
Fatoorehchi <tfatoo@gmail.com>
2020-06-18 11:35:19 +0200

    F - WelcomeButton[home] - buttons css done

--- frontend/src/components/Content/Body/Home/Welcome/Welcome.js
+++ frontend/src/components/Content/Body/Home/Welcome/Welcome.js
@@ -4,6 +4,8 @@
 */
 
 import React, { Fragment } from "react";
+import { NavLink } from 'react-router-dom';
+
 
 import Button from "../../../../UI/Button/Button";
 
@@ -16,8 +18,10 @@ const Welcome = props => {
             <h3 className="welcome__h3">1256 players connected on 678 rooms</h3>
             <p className="welcome__paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dictum justo vitae velit malesuada, ac eleifend velit interdum. Cras magna lacus, lacinia id egestas et, semper vitae mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             </p>
-            <Button btnClass="homebutton" submit="yes">Sign In !</Button>
-            <Button btnClass="homebutton" submit="yes">Sign Up !</Button>
+            <div className="divbuttonwelcome">
+                <NavLink to="/signin"><Button btnClass="welcomebutton" >Sign In !</Button></NavLink>
+                <NavLink to="/signup"><Button btnClass="welcomebutton" >Sign Up !</Button></NavLink>
+            </div>
         </Fragment>
     );
 };
